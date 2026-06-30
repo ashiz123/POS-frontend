a50777aa0eae6b57c83afba3b576cb8c2ecc96fa
ashiz123 <37142670+ashiz123@users.noreply.github.com>
2026-06-30 22:58:42 +0100

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,26 @@
-import { defineConfig } from "vite";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   server: {
+//     allowedHosts: ["dc40-88-97-218-39.ngrok-free.app"],
+//   },
+// });
+
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    allowedHosts: ["dc40-88-97-218-39.ngrok-free.app"],
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  // This helps you verify the value in the Netlify Build Log
+  console.log("DEBUG: VITE_API_URL is set to:", env.VITE_API_URL);
+
+  return {
+    plugins: [react(), tailwindcss()],
+  };
 });
