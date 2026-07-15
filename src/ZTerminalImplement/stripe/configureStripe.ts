a29c921f87916694f09f35a607ab9fa7c20c4ae9
Ashiz <ashizweb@gmail.com>
2026-07-15 14:54:37 +0100

    FIX: local static link to env link

--- src/ZTerminalImplement/stripe/configureStripe.ts
+++ src/ZTerminalImplement/stripe/configureStripe.ts
@@ -1,54 +1,55 @@
-import type { PaymentIntent } from './types'
+import type { PaymentIntent } from "./types";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export async function fetchConnectionToken(): Promise<string> {
-    return fetch('http://localhost:3000/stripe/connection_token', {
-        method: 'POST',
+  return fetch(`${API_URL}/stripe/connection_token`, {
+    method: "POST",
+  })
+    .then(function (response) {
+      return response.json();
     })
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data) {
-            return data.secret
-        })
+    .then(function (data) {
+      return data.secret;
+    });
 }
 
 export function unexpectedDisconnect() {
-    console.log('Disconnected from reader')
+  console.log("Disconnected from reader");
 }
 
 export async function discoverReaderHandler(terminal) {
-    const config = { simulated: true }
+  const config = { simulated: true };
 
-    const discoverResult = await terminal.discoverReaders(config)
+  const discoverResult = await terminal.discoverReaders(config);
 
-    if (discoverResult.error) {
-        console.log('Failed to discover')
-        return
-    }
+  if (discoverResult.error) {
+    console.log("Failed to discover");
+    return;
+  }
 
-    const readers = discoverResult.discoveredReaders || []
-    if (!readers.length) {
-        console.log('No available readers.')
-        return
-    }
+  const readers = discoverResult.discoveredReaders || [];
+  if (!readers.length) {
+    console.log("No available readers.");
+    return;
+  }
 
-    return readers[0]
+  return readers[0];
 }
 
 //if i want to
 export async function fetchPaymentIntent(
-    amount: number,
-    currency: string = 'usd'
+  amount: number,
+  currency: string = "usd",
 ): Promise<PaymentIntent> {
-    return fetch('http://localhost:3000/stripe/create_payment_intent', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ amount, currency }),
-    }).then(function (response) {
-        return response.json()
-    })
+  return fetch(`${API_URL}/stripe/create_payment_intent`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ amount, currency }),
+  }).then(function (response) {
+    return response.json();
+  });
 }
 
 // export async function notifyBackend(orderId,  PaymentIntent) {
